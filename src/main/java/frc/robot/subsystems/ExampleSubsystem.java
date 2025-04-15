3f34eb5e7e2a59245464f20bace253973be03232
ignacio-w <ignaciowijatno@gmail.com>
2025-04-14 23:13:43 -0500

    Initialized Motor Controllers
    Created a SparkMax motor controller

--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -4,10 +4,17 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ExampleSubsystem extends SubsystemBase {
+  
+  SparkMax motor = new SparkMax(1, MotorType.kBrushless);
+  
+  
   /** Creates a new ExampleSubsystem. */
   public ExampleSubsystem() {}
 
